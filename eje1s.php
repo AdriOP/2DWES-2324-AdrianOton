dca2a0d9925ea16d6bdc8abb21d9d6f0c040b55e
Adrian <99034770+AdriOP@users.noreply.github.com>
2023-09-20 11:04:37 +0200

    Create eje1s.php

---
+++ eje1s.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>EJ1-Conversion IP Decimal a Binario</title>
+</head>
+<body>
+    <?php
+    $ip = "192.18.16.204";
+    $num = explode(".", $ip);
+
+    $binario = "";
+    
+    list($n1, $n2, $n3, $n4) = $num;
+    
+    $binario .= str_pad(decbin($n1), 8, "0", STR_PAD_LEFT) . ".";
+    $binario .= str_pad(decbin($n2), 8, "0", STR_PAD_LEFT) . ".";
+    $binario .= str_pad(decbin($n3), 8, "0", STR_PAD_LEFT) . ".";
+    $binario .= str_pad(decbin($n4), 8, "0", STR_PAD_LEFT);
+	
+	  printf "La IP $ip en binario es   $binario <br/>";
+    ?>
+</body>
+</html>
