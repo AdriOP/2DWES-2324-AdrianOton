8e08cf371b7f28c400850e667e597fc73598612e
Adrian <99034770+AdriOP@users.noreply.github.com>
2024-02-12 10:50:13 +0100

    Create db.php

---
+++ movmad/db/db.php
@@ -0,0 +1,19 @@
+<?php
+echo "Inicio db.php"."<br>";
+
+$servername = "localhost";
+$username = "root";
+$password = "rootroot";
+$database = "movilmad";
+
+try {
+    $conexion = new PDO("mysql:host=$servername;dbname=$database", $username, $password); 	 	 	 	 	 	
+    $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 	 	 	 	 	 	
+} catch (PDOException $ex) {
+    echo $ex->getMessage(); 	 	 	 	 	 	
+}
+    
+echo "finaliza db.php"."<br>";
+
+return $conexion; // Importante: Devolver la conexión para que esté disponible en otros archivos.
+?>
